4bf133e53b30be15c926f272418684f7e3725d20
vo992 <vo992@DESKTOP-2IIJ2BS>
2024-06-16 21:54:53 +0300

    Add project files.

---
+++ Lab_3/ClassLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Bicycle
+    {
+        public string Model;
+        public string Frame;
+        public int Broadcast;
+        public string Fork;
+        public string Handlebars;
+        public bool Ring;
+        public bool Has3Wheels;
+
+        public int Cost()
+        {
+            return Broadcast * 1 / 2 * 100;
+        }
+
+        public int Value
+        {
+            get { return Cost(); }
+            set { }
+        }
+    }
+}
---
+++ Lab_3/fMain.cs
@@ -0,0 +1,44 @@
+﻿ using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class fMain : Form
+    { 
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddBicycle_Click(object sender, EventArgs e)
+        {
+            Bicycle bc = new Bicycle();
+            fBicycle fbc = new fBicycle(bc);
+            if(fbc.ShowDialog() == DialogResult.OK)
+            {
+                tbPhonesInfo.Text +=
+                $"Модель: {bc.Model}\n " +
+                $"Рама:{bc.Frame}\n " +
+                $"Передачі: {bc.Broadcast}\n " +
+                $"Вилка: {bc.Fork}\n " +
+                $"Має дзвінок: {(bc.Ring ? "має дзвінок" : "не має дзвінку")}\n " +
+                $"Має 3 колеса:{(bc.Has3Wheels ? "має 3 колеса" : "не має 3х колес")}\n ";
+            }
+
+        }
+
+        private void btnCloseClick(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Припинити роботу застосунку?", "Припинити роботу", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                Application.Exit();
+        }
+    }
+}
